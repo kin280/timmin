d59596a1f3fd5355802d65981b5bc6d20988bd5c
kienb <kienb@KIN>
2023-12-29 19:47:25 +0700

    Add project files.

---
+++ timmin/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+    
+            int[] arr = { 4, 12, 7, 8, 1, 6, 9 };
+            
+            Console.WriteLine("The smallest element in the array is: " + MinValue(arr));
+     }
+
+    static int MinValue(int[] array)
+    {
+
+        int min = array[0];
+      
+
+        for (int i = 0; i < array.Length-1; i++)
+        {
+            if (array[i] < min)
+            {
+                min = array[i];
+              
+            }
+        }
+
+        return min;
+    }
+
+
+
+
+
+
+
+}
